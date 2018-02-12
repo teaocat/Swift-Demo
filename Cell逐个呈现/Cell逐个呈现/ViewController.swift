24f29fb50adb5cac72ebb7acf672710c22a2f345
teaocat <teaocat@gmail.com>
2018-02-12 22:32:24 +0800

    tableView的Cell的弹出动画效果

---
+++ Cell逐个呈现/Cell逐个呈现/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Cell逐个呈现
+//
+//  Created by Teaocat on 2018/2/12.
+//  Copyright © 2018年 learning-swift. All rights reserved.
+//
+//  Github: https://github.com/teaocat
+//
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DispatchQueue.main.async {
+            [weak self] in self?.animateTable()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func animateTable() {
+        let cells = tableView.visibleCells
+        let tableHeight: CGFloat = tableView.bounds.height
+        for (index, cell) in cells.enumerated() {
+            cell.transform = CGAffineTransform(translationX: 0, y: tableHeight)
+            UIView.animate(withDuration: 1.0, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [], animations: {
+                cell.transform = CGAffineTransform(translationX: 0, y: 0)
+            }, completion: nil)
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+}
