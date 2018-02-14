ded3d8a3356a478adbce064a603896059e8dde45
teaocat <teaocat@gmail.com>
2018-02-14 22:21:43 +0800

    添加Cell点击动画

--- Cell逐个呈现/Cell逐个呈现/ViewController.swift
+++ Cell逐个呈现/Cell逐个呈现/ViewController.swift
@@ -29,9 +29,11 @@ class ViewController: UIViewController {
 
     func animateTable() {
         let cells = tableView.visibleCells
-        let tableHeight: CGFloat = tableView.bounds.height
+        //        let tableHeight: CGFloat = tableView.bounds.height
+        let tableWidth: CGFloat = tableView.bounds.width
         for (index, cell) in cells.enumerated() {
-            cell.transform = CGAffineTransform(translationX: 0, y: tableHeight)
+            //            cell.transform = CGAffineTransform(translationX: 0, y: tableHeight)
+            cell.transform = CGAffineTransform(translationX: tableWidth, y: 0)
             UIView.animate(withDuration: 1.0, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [], animations: {
                 cell.transform = CGAffineTransform(translationX: 0, y: 0)
             }, completion: nil)
@@ -47,5 +49,36 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = tableView.cellForRow(at: indexPath)
+        UIView.beginAnimations(nil, context: nil)
+        
+        UIView.setAnimationDuration(0.2)
+        
+        cell?.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        
+        UIView.commitAnimations()
+    }
+    
+    func tableView(_ tableView: UITableView, didUnhighlightRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        UIView.beginAnimations(nil, context: nil)
+        
+        UIView.setAnimationDuration(0.2) //设置动画时间
+        
+        cell?.transform =  CGAffineTransform(scaleX: 1.0, y: 1.0)
+        
+        UIView.commitAnimations()
     }
 }
